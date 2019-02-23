24f5c17eb140f95272bce9f208220dd9ddc47207
xzho2604 <erikxiangzhou@gmail.com>
2019-02-23 13:16:38 +1100

    change model

--- drive.py
+++ drive.py
@@ -61,6 +61,10 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        
+        #resize the image to 32,32,3
+        image_array =  tf.image.resize_images(image_array,(32,32))
+
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
--- model.py
+++ model.py
@@ -50,9 +50,9 @@ for layer in inception.layers:
     layer.trainable = False
 
 #resize the input image to 32,32,3
-cifar_input = Input(shape=(32,32,3))
+cifar_input = Input(shape=(160,320,3))
 resized_input = Lambda(lambda image: tf.image.resize_images(image, (160, 320)))(cifar_input)
-inp = inception(resized_input)
+inp = inception(cifar_input)
 
 x = GlobalAveragePooling2D()(inp)
 x = Dense(128, activation = 'relu')(x)
@@ -64,9 +64,9 @@ model.compile(optimizer='Adam', loss='mse', metrics=['accuracy'])
 
 #model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3))) #crop the input image
 #model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3))) #normalise the input images
-model.fit(X_train, y_train, validation_split = 0.2, shuffle = True,verbose=1,epochs = 10)
+model.fit(X_train, y_train, validation_split = 0.2, shuffle = True,verbose=1,epochs = 7)
 
-model.save('model.h5')
+model.save('model2.h5')
 
 
 
